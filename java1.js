bf4ea6ba92c30c89e693d0a1ed0e001e55244750
2emk7 <104653877+2emk7@users.noreply.github.com>
2022-11-02 08:13:45 -0400

    Create java1.js

---
+++ java1.js
@@ -0,0 +1,3 @@
+function java1() {
+  document.getElementById('output').innerHTML = javascript:function gcloak() { var link = document.querySelector(\"link[rel*='icon']\") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico';document.title = 'My Drive - Google Drive';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link) };gcloak();setInterval(gcloak, 1000);;
+}
